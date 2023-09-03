9559e932008cd8317357740b791e4eafce277267
ibra1987 <moi-meme10@hotmail.com>
2023-09-04 00:10:12 +0100

    linkedlist

---
+++ linkedLists/main.js
@@ -0,0 +1,118 @@
+// create a linked list class and a node class
+
+class Node{
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(value){
+        this.head = {
+            value:value,
+            next:null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
+
+    // implementing append method
+    append(value){
+        // instantiate a node object from the value and set it to point to null at first
+        const newNode = new Node(value)
+       
+      //we set the tail next node the new node
+        this.tail.next = newNode
+
+        // to keep track of the tail we reset the tail to hold the appendid value 
+        this.tail = newNode
+        //set the next node of the new inserted node which is now the tail to null
+        newNode.next = null
+        // increment linked list length to keep track of number of nodes
+        this.length++
+        
+        // return the linked list itself
+        return this
+
+    }
+
+    // implementing prepend method
+
+    prepend(value){
+        
+        const newNode = new Node(value)
+         
+        // save the head to a variable current
+        const current = this.head
+        // reset the head to the new node
+        this.head = newNode
+        //set the new node next node to the current 
+        newNode.next = current
+        // increment the length 
+        this.length++
+        // return the linked list itself
+        return this;
+    }
+
+    // get a node a specific index
+
+    getNodeByIndex(index){
+       
+        // perform some checks on the index
+        if(index<0){
+            index =0
+        }
+        if(index>this.length-1){
+            index = this.length-1
+        }
+        // create a variable and set it to 0 to keep track wheteher we have reached the index
+        let nodeCount =0;
+        //set the start of the loop to the head
+        let currentNode = this.head;
+        //loop through the linked list until we get the specified index 
+        while(nodeCount !== index){
+            
+            currentNode = currentNode.next
+            nodeCount++
+        }
+        //return the currentNode node
+        return currentNode
+    }
+    /* implementing insert method
+    in order to inset to a specified index 
+    we need to to know the node before and after the node at the specified index*/
+
+    insert(index,value){
+        /* performing checks on index
+        if index is less than 0 we reset it to 0
+        if index is more than length-1 we reset it to length-1
+        */
+        if(index<0){
+            index = 0
+        }
+        if(index>this.length-1){
+            index = this.length-1
+        }
+        // instatiate node class
+        const newNode = new Node(value)
+        // get the node before the required index
+        let nodeBeforeIndex = this.getNodeByIndex(index-1)
+        // get the element to move nextNode is actually the node at the specified index where we want to insert a new node
+        const nextNode = nodeBeforeIndex.next
+        nodeBeforeIndex.next =newNode
+        newNode.next = nextNode
+        this.length++;
+        return this
+    }
+    // implement deleting methof
+
+    remove(index){
+        //TODO inmplementing the deleting method
+    }
+}
+
+const mlk = new LinkedList(1)
+mlk.append(2)
+mlk.append(3)
+console.log(JSON.stringify(mlk.insert(1,7)))
